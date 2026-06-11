ab05012228c7b8e451fb15a004f5457cb50e12d9
Sanjay-Kumar345 <sanjaykumar345@gmail.com>
2026-06-11 23:04:56 +0530

    add strg

--- main.tf
+++ main.tf
@@ -1,5 +1,14 @@
-resource "azurerm_resource_group" "rgs" {
-    name = "alpha"
+resource "azurerm_resource_group" "rgs1" {
+    name = "beta"
     location = "Central US"
+  
+}
+
+resource "azurerm_storage_account" "strg" {
+    name = "alphastrg45"
+    location = azurerm_resource_group.rgs1.location
+    resource_group_name = azurerm_resource_group.rgs1.name
+    account_tier = "Standard"
+    account_replication_type = "ZRS"
   
 }
